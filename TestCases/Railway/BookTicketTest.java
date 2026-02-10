b326bc6ecc7feb7f8a49fa93c56abeaf6f3ba328
Gia Bao <nguyenluonggiabao301203@gmail.com>
2026-02-10 11:05:48 +0700

    Implement TC13

--- TestCases/Railway/BookTicketTest.java
+++ TestCases/Railway/BookTicketTest.java
@@ -65,5 +65,61 @@ public class BookTicketTest extends TestBase {
 		String actualDepartDate = successPage.getCellValueByHeader("Depart Date");
 		Assert.assertEquals(actualDepartDate, targetDate);
 	}
+	
+	@Test
+	public void TC13() {
+		System.out.println("Prepare data");
+		User activeUser = TestUtils.createActivatedAccount();
+		String departFrom = "Nha Trang";
+		String arriveAt = "Sài Gòn";
+		String seatType = "Soft seat with air conditioner";
+		String ticketAmount = "5";
+
+		String expectedMsg = "Ticket booked successfully!";
+
+		System.out.println("TC13 - User can book many tickets at a time");
+
+		System.out.println("1. Navigate to QA Railway Website");
+		HomePage homePage = new HomePage();
+		homePage.open();
+
+		System.out.println("2. Login with a valid account ");
+		LoginPage loginPage = homePage.gotoPage(MenuTab.LOGIN, LoginPage.class);
+		homePage = loginPage.login(activeUser, HomePage.class);
+
+		System.out.println("3. Click on \"Book ticket\" tab");
+		BookTicketPage bookTicketPage = homePage.gotoPage(MenuTab.BOOKTICKET, BookTicketPage.class);
+
+		System.out.println("4. Select the next 25 days from \"Depart date\"");
+		System.out.println("5. Select \"Nha Trang\" for \"Depart from\" and \"Sài Gòn\" for \"Arrive at\"");
+		System.out.println("6. Select \"Soft seat with air conditioner\" for \"Seat type\"");
+		System.out.println("7. Select \"5\" for \"Ticket amount\"");
+		System.out.println("8. Click on \"Book ticket\" button");
+		String currentDepartDate = bookTicketPage.getSelectedDepartDate();
+		String targetDate = Utilities.plusDaysFromCurrentDepartDate(currentDepartDate, 25);
+
+		BookTicketSuccessPage successPage = bookTicketPage.bookTicket(targetDate, departFrom, arriveAt, seatType, ticketAmount);
+
+		System.out.println("VP: Message \"Ticket booked successfully!\" displays.");
+		String actualMsg = successPage.getSuccessMsg();
+		Assert.assertEquals(actualMsg, expectedMsg, "Successful message is not displayed as expected");
+
+		System.out.println(
+				"VP: Ticket information display correctly (Depart Date,  Depart Station,  Arrive Station,  Seat Type,  Amount)");
+		String actualDepartStation = successPage.getCellValueByHeader("Depart Station");
+		Assert.assertEquals(actualDepartStation, departFrom);
+
+		String actualArriveStation = successPage.getCellValueByHeader("Arrive Station");
+		Assert.assertEquals(actualArriveStation, arriveAt);
+
+		String actualSeatType = successPage.getCellValueByHeader("Seat Type");
+		Assert.assertEquals(actualSeatType, seatType);
+
+		String actualAmount = successPage.getCellValueByHeader("Amount");
+		Assert.assertEquals(actualAmount, ticketAmount);
+
+		String actualDepartDate = successPage.getCellValueByHeader("Depart Date");
+		Assert.assertEquals(actualDepartDate, targetDate);
+	}
 
 }
